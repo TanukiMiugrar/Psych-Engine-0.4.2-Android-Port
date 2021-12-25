3da2a0b04d281b45c7b310ad0f751091e4edf2bb
TanukiMiugrar <96599613+TanukiMiugrar@users.noreply.github.com>
2021-12-25 12:04:35 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -196,7 +196,7 @@ class TitleState extends MusicBeatState
 		// bg.updateHitbox();
 		add(bg);
 
-		logoBl = new FlxSprite(120, -40;
+		logoBl = new FlxSprite(120, -41;
 		logoBl.frames = Paths.getSparrowAtlas('logoBumpin');
 		logoBl.antialiasing = ClientPrefs.globalAntialiasing;
 		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
